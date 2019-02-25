8feed49103d45dbc040f1d2b180839ec28595678
Thomas Antony <tantony@purdue.edu>
2019-02-25 15:01:35 -0600

    Initial homebrew formula

---
+++ qcl.rb
@@ -0,0 +1,17 @@
+class Qcl < Formula
+  desc "A command line tool to transfer files into Qconn-enabled remote device"
+  homepage "https://github.com/TheHipbot/weather"
+  url "https://github.com/thomasantony/qcl/archive/master.zip"
+  sha256 "db762174cd29e827007d97333853e64a8e39210ac4fad23f3083d9912a5ba3ce"
+  version "1.0.0"
+
+  depends_on "perl"
+  depends_on "cpanminus"
+
+  bottle :unneeded
+
+  def install
+    system "cpanm", "Net::Telnet"
+    bin.install "qcl"
+  end
+end
